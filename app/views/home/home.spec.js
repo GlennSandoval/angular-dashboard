be50a0758b80bf181415fe39ba388dc4af21e451
Glenn Sandoval <glenn.sandoval@greentree.com>
2016-07-31 18:05:40 +1200

    add bower deps

---
+++ app/views/home/home.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('snDash.home module', function() {
+
+    beforeEach(module('snDash.home'));
+
+    describe('home controller', function() {
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var view2Ctrl = $controller('HomeCtrl');
+            expect(view2Ctrl).toBeDefined();
+        }));
+
+    });
+});
